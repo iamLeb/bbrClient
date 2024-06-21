7fbf9a55161a318f461ff4d91121e0a9f40a5d6b
Caleb Joseph <calebbjoseph@gmail.com>
2024-06-21 10:53:33 -0500

    pull update from BBR-23

--- src/pages/front/Home.jsx
+++ src/pages/front/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Hero from "../../components/FrontComponents/Hero.jsx";
 
 const Home = () => {
     return (
         <div>
             Home page
+            <Hero />
         </div>
     );
 };
