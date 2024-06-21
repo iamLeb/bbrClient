27e3b1f62fe3579cd02782b36b0b58c80e9df31f
Caleb Joseph <calebbjoseph@gmail.com>
2024-06-21 09:42:30 -0500

    updated Home page

--- src/pages/front/Home.jsx
+++ src/pages/front/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Sale from "../../components/FrontComponents/Sale.jsx";
+import Featured from "../../components/FrontComponents/Featured.jsx";
 
 const Home = () => {
     return (
         <div>
             Home page
+            <Featured />
             <Sale />
         </div>
     );
