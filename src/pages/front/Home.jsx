9a4abd3d225faf2d44365f5e4777d2cbb6a33c60
Caleb Joseph <calebbjoseph@gmail.com>
2024-06-21 09:21:55 -0500

    BBR-26 completing rent section

--- src/pages/front/Home.jsx
+++ src/pages/front/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Featured from "../../components/FrontComponents/Featured.jsx";
+import Rent from "../../components/FrontComponents/Rent.jsx";
 
 const Home = () => {
     return (
         <div>
             Home page
-            <Featured />
+            <Rent />
         </div>
     );
 };
