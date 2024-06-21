565a80e6d0cce12548d260fb84e161644dda4e65
Caleb Joseph <calebbjoseph@gmail.com>
2024-06-21 14:14:57 -0500

    BBR-29 Created the footer section

--- src/pages/front/Home.jsx
+++ src/pages/front/Home.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from "../../components/FrontComponents/Hero.jsx";
 import Featured from "../../components/FrontComponents/Featured.jsx";
 import Explore from "../../components/FrontComponents/Explore.jsx";
-import Sale from "../../components/FrontComponents/Sale.jsx";
 import Rent from "../../components/FrontComponents/Rent.jsx";
 
 const Home = () => {
@@ -11,7 +10,6 @@ const Home = () => {
             <Hero />
             <Featured />
             <Explore />
-            <Sale />
             <Rent />
         </div>
     );
