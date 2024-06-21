6f77f1e02eff7243a09e5910937e01d0aed51705
EyambaV <valour.francis@gmail.com>
2024-06-21 10:36:15 -0500

    BBR-19 changed sizes of picture and changed from flex to grid

--- src/components/FrontComponents/Explore.jsx
+++ src/components/FrontComponents/Explore.jsx
@@ -1,106 +1,110 @@
-import neigh from "../../assets/images/neighbourhoodpic.jpg"
+import one from "../../assets/images/neighbourhoodpic.jpg"
+import two from "../../assets/images/testingdiv.jpg"
+import three from "../../assets/images/explore3.jpg"
+import four from "../../assets/images/explore4.jpg"
+import five from "../../assets/images/explore5.jpg"
+import six from "../../assets/images/explore6.jpg"
+import seven from "../../assets/images/explore7.jpg"
+import eight from "../../assets/images/explore8.jpg"
 import { PiPlusThin } from "react-icons/pi";
 
 
 const Explore = () => {
     return (
         <div>
-            <div className="text-xl xs-text-3xl text-center sm:text-5xl font-bold xs:p-10 sm:p-20">Discover Different Neighbourhoods</div>
-            <div className="sm:flex sm:h-64 sm:overflow-hidden">
-                <div className="group sm:h-full sm:w-1/4 relative"> 
-                    <div className="overflow-hidden bg-black h-full sm:absolute sm:inset-0">
-                        <img className="object-cover h-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={neigh} alt =''/>
-                    </div>
-                    
+            <div className="text-center font-bold text-xl sm:text-3xl p-[30px]">Discover The Perfect Neighbourhood</div>
+            <div className="grid grid-cols-1 md:grid-cols-2 h-[160rem] lg:grid-cols-4 md:h-[75rem] lg:h-[35rem]">
+                <div className="group h-full w-full relative overflow-hidden"> 
+                    <div className="overflow-hidden w-full bg-black h-full sm:absolute sm:inset-0">
+                        <img className="object-cover h-full w-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={one} alt =''/>
+                    </div> 
                     <div className=" sm:flex flex-col absolute bottom-0 justify-end p-5 sm:h-full bg-opacity-0 sm:relative text-white">
                         <p>St Vital</p>
-                        <p className="text-xs">4 properties</p>
-                    </div>
-                    <div className="transition-all duration-500 h-full flex flex-col justify-center items-center absolute inset-0 opacity-0 group-hover:opacity-100 p-5 sm:inset-0 text-white">
-                        <PiPlusThin className="text-6xl"/>
-                    </div>
-                </div>
-                <div className="group sm:h-full sm:w-1/4 relative"> 
-                    <div className="overflow-hidden bg-black h-full sm:absolute sm:inset-0">
-                        <img className="object-cover h-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={neigh} alt =''/>
-                    </div>
-                    <div className=" sm:flex flex-col absolute bottom-0 justify-end p-5 sm:h-full bg-opacity-0 sm:relative text-white">
-                        <p>Sage Creek</p>
-                        <p className="text-xs">5 properties</p>
-                    </div>
-                    <div className="transition-all duration-500 h-full flex flex-col justify-center items-center absolute inset-0 opacity-0 group-hover:opacity-100 p-5 sm:inset-0 text-white">
-                        <PiPlusThin className="text-6xl"/>
-                    </div>
-                </div>
-                <div className="group sm:h-full sm:w-1/4 relative"> 
-                    <div className="overflow-hidden bg-black h-full sm:absolute sm:inset-0">
-                        <img className="object-cover h-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={neigh} alt =''/>
-                    </div>
-                    <div className=" sm:flex flex-col absolute bottom-0 justify-end p-5 sm:h-full bg-opacity-0 sm:relative text-white">
-                        <p>Island Lakes</p>
                         <p className="text-xs">3 properties</p>
                     </div>
                     <div className="transition-all duration-500 h-full flex flex-col justify-center items-center absolute inset-0 opacity-0 group-hover:opacity-100 p-5 sm:inset-0 text-white">
                         <PiPlusThin className="text-6xl"/>
                     </div>
                 </div>
-                <div className="group sm:h-full sm:w-1/4 relative"> 
-                    <div className="overflow-hidden bg-black h-full sm:absolute sm:inset-0">
-                        <img className="object-cover h-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={neigh} alt =''/>
-                    </div>
+                <div className="group sm:h-full sm:w-full relative overflow-hidden"> 
+                    <div className="overflow-hidden w-full bg-black h-full sm:absolute sm:inset-0">
+                        <img className="object-cover h-full w-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={two} alt =''/>
+                    </div> 
                     <div className=" sm:flex flex-col absolute bottom-0 justify-end p-5 sm:h-full bg-opacity-0 sm:relative text-white">
-                        <p>St Vital</p>
+                        <p>St James</p>
+                        <p className="text-xs">6 properties</p>
+                    </div>
+                    <div className="transition-all duration-500 h-full flex flex-col justify-center items-center absolute inset-0 opacity-0 group-hover:opacity-100 p-5 sm:inset-0 text-white">
+                        <PiPlusThin className="text-6xl"/>
+                    </div>
+                </div>
+                <div className="group sm:h-full sm:w-full relative overflow-hidden"> 
+                    <div className="overflow-hidden w-full bg-black h-full sm:absolute sm:inset-0">
+                        <img className="object-cover h-full w-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={three} alt =''/>
+                    </div> 
+                    <div className=" sm:flex flex-col absolute bottom-0 justify-end p-5 sm:h-full bg-opacity-0 sm:relative text-white">
+                        <p>Tuxedo</p>
+                        <p className="text-xs">2 properties</p>
+                    </div>
+                    <div className="transition-all duration-500 h-full flex flex-col justify-center items-center absolute inset-0 opacity-0 group-hover:opacity-100 p-5 sm:inset-0 text-white">
+                        <PiPlusThin className="text-6xl"/>
+                    </div>
+                </div>
+                <div className="group sm:h-full sm:w-full relative overflow-hidden"> 
+                    <div className="overflow-hidden w-full bg-black h-full sm:absolute sm:inset-0">
+                        <img className="object-cover h-full w-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={four} alt =''/>
+                    </div> 
+                    <div className=" sm:flex flex-col absolute bottom-0 justify-end p-5 sm:h-full bg-opacity-0 sm:relative text-white">
+                        <p>Down Town</p>
                         <p className="text-xs">4 properties</p>
                     </div>
                     <div className="transition-all duration-500 h-full flex flex-col justify-center items-center absolute inset-0 opacity-0 group-hover:opacity-100 p-5 sm:inset-0 text-white">
                         <PiPlusThin className="text-6xl"/>
                     </div>
                 </div>
-            </div>
-            <div className="sm:flex sm:h-64 sm:overflow-hidden">
-                <div className="group sm:h-full sm:w-1/4 relative"> 
-                    <div className="overflow-hidden bg-black h-full sm:absolute sm:inset-0">
-                        <img className="object-cover h-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={neigh} alt =''/>
-                    </div>
+                <div className="group sm:h-full sm:w-full relative overflow-hidden"> 
+                    <div className="overflow-hidden w-full bg-black h-full sm:absolute sm:inset-0">
+                        <img className="object-cover h-full w-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={five} alt =''/>
+                    </div> 
                     <div className=" sm:flex flex-col absolute bottom-0 justify-end p-5 sm:h-full bg-opacity-0 sm:relative text-white">
-                        <p>Fort Richmond</p>
-                        <p className="text-xs">4 properties</p>
+                        <p>St Boniface</p>
+                        <p className="text-xs">1 propertys</p>
                     </div>
                     <div className="transition-all duration-500 h-full flex flex-col justify-center items-center absolute inset-0 opacity-0 group-hover:opacity-100 p-5 sm:inset-0 text-white">
                         <PiPlusThin className="text-6xl"/>
                     </div>
                 </div>
-                <div className="group sm:h-full sm:w-1/4 relative"> 
-                    <div className="overflow-hidden bg-black h-full sm:absolute sm:inset-0">
-                        <img className="object-cover h-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={neigh} alt =''/>
+                <div className="group sm:h-full sm:w-full relative overflow-hidden"> 
+                    <div className="overflow-hidden w-full bg-black h-full sm:absolute sm:inset-0">
+                        <img className="object-cover h-full w-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={six} alt =''/>
+                    </div> 
+                    <div className=" sm:flex flex-col absolute bottom-0 justify-end p-5 sm:h-full bg-opacity-0 sm:relative text-white">
+                        <p>Transcona</p>
+                        <p className="text-xs">2 properties</p>
                     </div>
+                    <div className="transition-all duration-500 h-full flex flex-col justify-center items-center absolute inset-0 opacity-0 group-hover:opacity-100 p-5 sm:inset-0 text-white">
+                        <PiPlusThin className="text-6xl"/>
+                    </div>
+                </div>
+                <div className="group sm:h-full sm:w-full relative overflow-hidden"> 
+                    <div className="overflow-hidden w-full bg-black h-full sm:absolute sm:inset-0">
+                        <img className="object-cover h-full w-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={seven} alt =''/>
+                    </div> 
+                    <div className=" sm:flex flex-col absolute bottom-0 justify-end p-5 sm:h-full bg-opacity-0 sm:relative text-white">
+                        <p>South Point</p>
+                        <p className="text-xs">7 properties</p>
+                    </div>
+                    <div className="transition-all duration-500 h-full flex flex-col justify-center items-center absolute inset-0 opacity-0 group-hover:opacity-100 p-5 sm:inset-0 text-white">
+                        <PiPlusThin className="text-6xl"/>
+                    </div>
+                </div>
+                <div className="group sm:h-full sm:w-full relative overflow-hidden"> 
+                    <div className="overflow-hidden w-full bg-black h-full sm:absolute sm:inset-0">
+                        <img className="object-cover h-full w-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={eight} alt =''/>
+                    </div> 
                     <div className=" sm:flex flex-col absolute bottom-0 justify-end p-5 sm:h-full bg-opacity-0 sm:relative text-white">
                         <p>Bridgewater</p>
-                        <p className="text-xs">1 property</p>
-                    </div>
-                    <div className="transition-all duration-500 h-full flex flex-col justify-center items-center absolute inset-0 opacity-0 group-hover:opacity-100 p-5 sm:inset-0 text-white">
-                        <PiPlusThin className="text-6xl"/>
-                    </div>
-                </div>
-                <div className="group sm:h-full sm:w-1/4 relative"> 
-                    <div className="overflow-hidden bg-black h-full sm:absolute sm:inset-0">
-                        <img className="object-cover h-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={neigh} alt =''/>
-                    </div>
-                    <div className=" sm:flex flex-col absolute bottom-0 justify-end p-5 sm:h-full bg-opacity-0 sm:relative text-white">
-                        <p>Richmond West</p>
-                        <p className="text-xs">4 properties</p>
-                    </div>
-                    <div className="transition-all duration-500 h-full flex flex-col justify-center items-center absolute inset-0 opacity-0 group-hover:opacity-100 p-5 sm:inset-0 text-white">
-                        <PiPlusThin className="text-6xl"/>
-                    </div>
-                </div>
-                <div className="group sm:h-full sm:w-1/4 relative"> 
-                    <div className="overflow-hidden bg-black h-full sm:absolute sm:inset-0">
-                        <img className="object-cover h-full transition-all duration-500 group-hover:scale-110 group-hover:opacity-50" src={neigh} alt =''/>
-                    </div>
-                    <div className=" sm:flex flex-col absolute bottom-0 justify-end p-5 sm:h-full bg-opacity-0 sm:relative text-white">
-                        <p>Pembina</p>
-                        <p className="text-xs">4 properties</p>
+                        <p className="text-xs">5 properties</p>
                     </div>
                     <div className="transition-all duration-500 h-full flex flex-col justify-center items-center absolute inset-0 opacity-0 group-hover:opacity-100 p-5 sm:inset-0 text-white">
                         <PiPlusThin className="text-6xl"/>
@@ -108,6 +112,7 @@ const Explore = () => {
                 </div>
             </div>
         </div>
+        
     );
 };
 
