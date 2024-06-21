73a09f5a164c1142e9e72d0459d329ae587afbcb
Caleb Joseph <calebbjoseph@gmail.com>
2024-06-21 10:43:38 -0500

    clear main home content

--- src/pages/front/Home.jsx
+++ src/pages/front/Home.jsx
@@ -1,7 +1,3 @@
-import Sale from "../../components/FrontComponents/Sale.jsx";
-import Featured from "../../components/FrontComponents/Featured.jsx";
-import Rent from "../../components/FrontComponents/Rent.jsx";
-
 const Home = () => {
     return (
         <div>
