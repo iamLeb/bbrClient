6340133b24c8e4f7cda286b45ff09b0dfe0cd015
EyambaV <valour.francis@gmail.com>
2024-06-27 09:34:28 -0500

    BBR-42 made registration page more responsive

--- src/components/FrontComponents/Auth/RegistrationHero.jsx
+++ src/components/FrontComponents/Auth/RegistrationHero.jsx
@@ -2,38 +2,38 @@ import one from "../../../assets/images/Register.jpg"
 
 const RegistrationHero = () => {
     return (
-        <div className="flex items-center justify-between mx-4 gap-x-5">
-            <div className="w-1/2  text-center">
-                <img src={one}/>
-            </div>
-            <div className="w-1/2 rounded-lg p-5 space-y-7 ">
-                <div className="">
-                    <h1 className="font-bold text-3xl text-center">Sign up</h1>
-                    <p className="text-center">join our family</p>
+        <div className="grid grid-cols-1 lg:grid-cols-2 justify-between m-20 gap-x-5">
+                <div className="hidden w-full object-cover  lg:flex items-center h-full ">
+                    <img className="w-full rounded-lg " src={one} alt=""/>
                 </div>
-                <div>
-                    <p>Name</p>
-                    <input type='text' placeholder="name" className="border w-full py-4 px-9 rounded-lg"/>
-                </div>
-                <div>
-                    <p>E-mail</p>
-                    <input type='text' placeholder="email" className="border w-full py-4 px-9 rounded-lg"/>
-                </div>
-                <div>
-                    <p>Password</p>
-                    <input type='text' placeholder="password" className="border w-full py-4 px-9 rounded-lg"/>
-                </div>
-                <div>
-                    <p>Phone number</p>
-                    <input type='text' placeholder="phone" className="border w-full py-4 px-9 rounded-lg"/>
-                </div>
-                <div className="flex justify-center">
-                    <div className="w-1/3 bg-primary p-3 flex justify-center rounded-lg text-bold text-white">
-                        Sign up
+                <div className="w-full p-5 space-y-7 ">
+                    <div className="">
+                        <h1 className="font-bold text-3xl text-center">Sign up</h1>
+                        <p className="text-center">join our family</p>
                     </div>
-                    
+                    <div>
+                        <p>Name</p>
+                        <input type='text' placeholder="name" className="border w-full py-4 px-9 rounded-lg"/>
+                    </div>
+                    <div>
+                        <p>E-mail</p>
+                        <input type='text' placeholder="email" className="border w-full py-4 px-9 rounded-lg"/>
+                    </div>
+                    <div>
+                        <p>Password</p>
+                        <input type='text' placeholder="password" className="border w-full py-4 px-9 rounded-lg"/>
+                    </div>
+                    <div>
+                        <p>Phone number</p>
+                        <input type='text' placeholder="phone" className="border w-full py-4 px-9 rounded-lg"/>
+                    </div>
+                    <div className="flex justify-center">
+                        <div className="w-1/3 bg-primary p-3 flex justify-center rounded-lg text-bold text-white">
+                            Sign up
+                        </div>
+                        
+                    </div>
                 </div>
-            </div>
         </div>
     )
 }
