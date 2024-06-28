4c78b32c823f994038cfcc7504d7fb2c5d26ff4b
EyambaV <valour.francis@gmail.com>
2024-06-28 11:44:17 -0500

    BBR-47 added table where you can add listings and remove listings from database

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Register from "./pages/front/auth/register.jsx";
 import ContactUs from "./pages/front/ContactUs.jsx";
 import DashboardLayout from "./layouts/DashboardLayout.jsx";
 import Admin from "./pages/dashboard/Admin.jsx";
+import TableComponent from "./components/DashboardComponents/TableComponent.jsx";
 
 function App() {
   return (
@@ -40,7 +41,8 @@ function App() {
 
 
           <Route path="/secure" element={<DashboardLayout />}>
-            <Route index element={<Admin />} />
+            <Route index element={<TableComponent />} />
+                <Route path={'/secure/properties'} element={<TableComponent />} />
 
           </Route>
         </Routes>
