1703ebd877fa7ed16514f85abaefeda49a12180a
Caleb Joseph <calebbjoseph@gmail.com>
2024-07-02 15:33:12 -0500

    added mobile responsive

--- src/components/FrontComponents/Hero.jsx
+++ src/components/FrontComponents/Hero.jsx
@@ -6,15 +6,15 @@ const Hero = () => {
             <div
                 className={'w-full h-[600px] bg-cover bg-center bg-[url("https://tunatheme.com/tf/html/quarter-preview/quarter/img/slider/13.jpg")] relative'}>
                 {/* Overlay */}
-                <div className="absolute inset-0 bg-neutral-900 opacity-50"></div>
+                {/*<div className="absolute inset-0 bg-neutral-900 opacity-50"></div>*/}
 
-                <div className={'flex justify-center items-center h-[600px] text-white relative'}>
-                    <div className={'flex flex-col gap-2 items-center'}>
+                <div className={'flex justify-center items-center h-[400px] md:h-[600px] text-white relative '}>
+                    <div className={'flex flex-col gap-2 items-center text-center'}>
                         <div className={'flex space-x-4 items-center'}>
                             <FaHouseChimney/>
                             <p>Real Estate Agency</p>
                         </div>
-                        <h1 className={'text-6xl text-center font-bold'}>Find your dream <br/> house by us</h1>
+                        <h1 className={'text-4xl md:text-6xl text-center font-bold'}>Find your dream <br/> house by us</h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia quaerat veritatis
                             voluptas?</p>
                         <button className={'bg-primary text-white p-4'}>Schedule Appointment</button>
@@ -23,7 +23,7 @@ const Hero = () => {
 
                 <div
                     className={'absolute bottom-0 translate-y-1/2 right-1/2 transform translate-x-1/2 container mx-auto bg-white shadow-lg p-9'}>
-                    <div className={'flex space-x-4'}>
+                    <div className={'md:flex space-y-4 md:space-y-0 md:space-x-4'}>
                         <select className={'px-5 border py-4 w-full rounded-lg'}>
                             <option selected={true}>Select Province</option>
                         </select>
@@ -32,7 +32,7 @@ const Hero = () => {
                         </select><select className={'px-5 border py-4 w-full rounded-lg'}>
                             <option selected={true}>Neighborhood</option>
                         </select>
-                        <button className={'bg-primary text-white w-full'}>Find Now</button>
+                        <button className={'bg-primary text-white h-14 rounded-lg w-full'}>Find Now</button>
                     </div>
                 </div>
             </div>
