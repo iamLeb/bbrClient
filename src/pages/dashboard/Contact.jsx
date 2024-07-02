af121e6f8430cbab54081d804a42c4f8e2160623
Faiza <salami.faiza15@gmail.com>
2024-07-02 11:58:40 -0500

    BBR-53 Created the contact dahsboard page and connected it to the database

--- src/pages/dashboard/Contact.jsx
+++ src/pages/dashboard/Contact.jsx
@@ -1,43 +1,51 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import api from "../../services/api.js";
 
 const Contact = () => {
-    const [contacts, setContacts] = useState([
-        {
-            name: 'Faiza',
-            email: 'f@gmail.com',
-            phone: '431278288222',
-            message: 'Hello I was revyil.vuwiubduobdbuwdjjobdjbdw'
-        },
-        {
-            name: 'Victor',
-            email: 'vic@gail.com',
-            phone: '792812892189982198',
-            message: 'Hello I was revyil.vuwiubduobdbuwdjjobdjbdw'
-        },
-        {
-            name: 'Faiza',
-            email: 'jinssu@ghshsjjs',
-            phone: '431278288222',
-            message: 'Hello I was revyil.vuwiubduobdbuwdjjobdjbdw'
-        },
-        {
-            name: 'FaRandoiza',
-            email: 'jinssu@ghshsjjs',
-            phone: '431278288222',
-            message: 'Call me when yoire avaianlele'
-        },
-    ]);
+    const [contacts, setContacts] = useState([]);
+    const [errors, setErrors] = useState('');
 
+    const fetchContacts = async () => {
+        try {
+            const res = await api.get('/contact');
+            setContacts(res.data);
+        } catch (e) {
+            setErrors('There was an error fetching contacts');
+        }
+    }
 
+    useEffect(() => {
+        fetchContacts()
+    });
+
+    const handleDelete = async id => {
+        try {
+            const res = await api.delete(`/contact/${id}`);
+            if (res.status === 200) {
+                setContacts(contacts.filter(contact => contact._id !== id));
+            }
+        } catch (e) {
+            setErrors('There was an error deleting the contact');
+        }
+    }
     return (
         <section className="h-screen m-5 mx-10">
-            <div className={'container mx-auto gap-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'}>
+            <div className={'gap-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'}>
                 {contacts.map((contact) => (
-                    <div className={'shadow shadow-lg p-4'} key={contact._id}>
-                        <h1>{contact.name}</h1>
-                        <p>{contact.email}</p>
-                        <p>{contact.phone}</p>
-                        <p>{contact.message}</p>
+                    <div className={'container mx-auto shadow shadow-lg p-4 rounded-lg flex flex-col space-y-4 items-center'} key={contact._id}>
+                        <div>
+                            <h1 className={'font-semibold'}>{contact.name}</h1>
+                            <p className={'text-sm'}>{contact.email}</p>
+                            <p className={'text-sm'}>{contact.phone}</p>
+                            <br/>
+                            <p>{contact.message}</p>
+                        </div>
+
+                        <div>
+                            <button onClick={() => handleDelete(contact._id)}
+                                    className="ml-2 px-2 py-1 rounded bg-red-500 text-white">Remove
+                            </button>
+                        </div>
                     </div>
                 ))}
             </div>
