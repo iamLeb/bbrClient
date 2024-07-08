94d02fe4f10bbf49df30fce9604294e47c52b737
Caleb Joseph <calebbjoseph@gmail.com>
2024-07-08 10:33:25 -0500

    push new remote base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_CORS,
+    baseURL: "https://bbrapi.onrender.com",
     withCredentials: true // Send cookies with cross-origin requests
 });
 
