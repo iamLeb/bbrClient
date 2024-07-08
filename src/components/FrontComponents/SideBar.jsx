9c51646290d096aebde4997b6880af11f6fd68a6
Faiza <salami.faiza15@gmail.com>
2024-07-08 15:34:27 -0500

    BBR-64 Changed the transition from

--- src/components/FrontComponents/SideBar.jsx
+++ src/components/FrontComponents/SideBar.jsx
@@ -52,7 +52,7 @@ const SideBar = ({sidebar, toggleSidebar}) => {
 
     return (
         <aside
-            className={`overflow-y-auto fixed lg:static lg:hidden top-0 left-0 bg-neutral-800 text-white h-screen w-80 sm:w-[500px] lg:w-80 transition-transform duration-500 ease-in-out py-2
+            className={`overflow-y-auto fixed lg:static lg:hidden top-0 left-0 bg-neutral-800 text-white h-screen w-80 sm:w-[500px] lg:w-80 transition-transform duration-300 ease-in-out py-2
             ${sidebar ? 'translate-x-0' : '-translate-x-full'} lg:transform-none`}>
 
             <div className="py-5 text-center border-b border-gray-50 flex items-center space-x-20">
