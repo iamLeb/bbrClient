dac2ec52130dbdae3cfd8e06450b44f33cc17bcf
Faiza <salami.faiza15@gmail.com>
2024-07-09 15:04:59 -0500

    BBR-34 Changed the contact page on homepage

--- src/components/FrontComponents/ContactHero.jsx
+++ src/components/FrontComponents/ContactHero.jsx
@@ -11,11 +11,11 @@ import {RiMailSendLine} from "react-icons/ri";
 const ContactHero = () => {
     return (
         <section className={'py-9 px-5 bg-[#d5eaea]'}>
-            <div className={'flex flex-col justify-around items-center pt-12 space-y-10'}>
+            <div className={'flex flex-col md:flex-row justify-around items-center pt-12 space-y-10'}>
 
-                <div className={'flex flex-col space-y-12 sm:pl-20 '}>
+                <div className={'md:w-1/2 flex flex-col space-y-12 sm:pl-20 '}>
                     <div className={'flex flex-col justify-center w-full sm:w-1/2 text-black'}>
-                        <h1 className={'  text-3xl md:text-4xl lg:text-5xl font-semibold'}>We Provide The Most
+                        <h1 className={'text-3xl lg:text-4xl font-semibold'}>We Provide The Most
                             Suitable And Quality Real
                             Estate.</h1>
                         <p className={'font-light'}>Our team of experts will help you find the best property that suits
@@ -76,53 +76,54 @@ const ContactHero = () => {
                         </div>
 
                         <form className={'flex flex-col gap-2'}>
-                            <div className={'pt-2'}>
-                                <div className={'font-medium pt-2'}>Your name</div>
-                                <div className={'relative'}>
+                            <div className={'flex flex-col md:flex-row pt-2 space-x-4'}>
+                                <div className={''}>
+                                    <div className={'font-medium pt-2'}>Your name</div>
+                                    <div className={'relative'}>
                                     <span
                                         className={'absolute text-primary flex items-center top-1/2 -translate-y-1/2 left-1'}>  <FaRegUser
                                         size={18}/></span>
-                                    <input
-                                        className={'px-9 focus:outline-primary border rounded-lg py-4 w-full placeholder:text-gray-500 text-sm'}
-                                        type="text" placeholder={"Your name"}/>
+                                        <input
+                                            className={'px-9 focus:outline-primary border rounded-lg py-4 w-full placeholder:text-gray-500 text-sm'}
+                                            type="text" placeholder={"Your name"}/>
+                                    </div>
                                 </div>
-                            </div>
 
 
-                            <div className={'flex-1'}>
-                                <div className={'font-medium pt-2'}>Email</div>
-                                <div className={'relative'}>
+                                <div className={''}>
+                                    <div className={'font-medium pt-2'}>Email</div>
+                                    <div className={'relative'}>
                                          <span
                                              className={'absolute text-primary flex items-center top-1/2 -translate-y-1/2 left-1'}>  <MdOutlineEmail
                                              size={18}/></span>
-                                    <input
-                                        className={'px-9 focus:outline-primary border rounded-lg py-4 w-full placeholder:text-gray-500 text-sm'}
-                                        type="text" placeholder={"Your email"}/>
+                                        <input
+                                            className={'px-9 focus:outline-primary border rounded-lg py-4 w-full placeholder:text-gray-500 text-sm'}
+                                            type="text" placeholder={"Your email"}/>
+                                    </div>
                                 </div>
                             </div>
 
-                            <div className={'flex-1'}>
-                                <div className={'font-medium pt-2'}>Phone</div>
-                                <div className={'relative'}>
+                            <div className={'flex flex-col md:flex-row pt-2 space-x-4'}>
+                                <div className={''}>
+                                    <div className={'font-medium pt-2'}>Phone</div>
+                                    <div className={'relative'}>
                                         <span
                                             className={'absolute text-primary flex items-center top-1/2 -translate-y-1/2 left-1'}> <FaPhoneAlt
                                             size={18}/></span>
-                                    <input
-                                        className={'px-9 focus:outline-primary border rounded-lg py-4 w-full placeholder:text-gray-500 text-sm'}
-                                        type="text" placeholder={"Your phone"}/>
+                                        <input
+                                            className={'px-9 focus:outline-primary border rounded-lg py-4 w-full placeholder:text-gray-500 text-sm'}
+                                            type="text" placeholder={"Your phone"}/>
+                                    </div>
                                 </div>
-                            </div>
 
 
-                            <div className={'flex-1'}>
-                                <div className={'font-medium pt-2 w-full'}>Property Type</div>
-                                <div className={'relative'}>
-                                    {/*<span*/}
-                                    {/*    className={'absolute text-primary flex items-center top-1/2 -translate-y-1/2 left-1'}> <TiHomeOutline*/}
-                                    {/*    size={18}/></span>*/}
-                                    <ul>
-                                        <PropertyType className={'px-9 py-4'}/>
-                                    </ul>
+                                <div className={'w-1/2'}>
+                                    <div className={'font-medium pt-2 w-full'}>Property Type</div>
+                                    <div className={'relative'}>
+                                        <ul>
+                                            <PropertyType className={'px-9 py-4'}/>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
 
