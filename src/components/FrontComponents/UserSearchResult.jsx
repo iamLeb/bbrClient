c7ffb916c1c1da86b7cf6f3dc1f18c619868f6e2
Faiza <salami.faiza15@gmail.com>
2024-07-11 09:41:21 -0500

    BBR-70 Created the user search result page

---
+++ src/components/FrontComponents/UserSearchResult.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const UserSearchResult = () => {
+    return (
+        <div>
+        </div>
+    );
+};
+
+export default UserSearchResult;
--- src/pages/front/Home.jsx
+++ src/pages/front/Home.jsx
@@ -5,12 +5,14 @@ import Explore from "../../components/FrontComponents/Explore.jsx";
 import Testimonials from '../../components/FrontComponents/Testimonials.jsx';
 import Contact from "../../components/FrontComponents/Contact.jsx";
 import RegistrationHero from '../../components/FrontComponents/Auth/RegistrationHero.jsx';
+import UserSearchResult from "../../components/FrontComponents/UserSearchResult.jsx";
 
 
 const Home = () => {
     return (
         <div>
             <Hero />
+            <UserSearchResult/>
             <Featured />
             <Explore />
             <Contact />
