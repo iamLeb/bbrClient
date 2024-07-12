725ef958e282572ad8178e138be786114ec7b99f
Faiza <salami.faiza15@gmail.com>
2024-07-12 10:39:17 -0500

    BBR-75 Explore page gallery comes from the database now

--- src/context/GlobalContextProvider.jsx
+++ src/context/GlobalContextProvider.jsx
@@ -1,8 +1,8 @@
 import GlobalContext from './Global.js';
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import api from "../services/api.js";
 
-const GlobalContextProvider = ({ children }) => {
+const GlobalContextProvider = ({children}) => {
     const [loading, setLoading] = useState(false);
 
     const [categories, setCategories] = useState([]);
@@ -10,6 +10,7 @@ const GlobalContextProvider = ({ children }) => {
     const [blogs, setBlogs] = useState([]);
     const [neighbourhoods, setNeighbourhoods] = useState([]);
     const [contacts, setContacts] = useState([]);
+    const [galleries, setGalleries] = useState([]);
 
     const getCategories = async () => {
         const res = await api.get('category');
@@ -31,13 +32,31 @@ const GlobalContextProvider = ({ children }) => {
             blogsData.map(async (blog) => {
                 const mediaResponse = await fetchMedia(blog._id);
                 const url = mediaResponse.data.url ?? 'default.png';
-                return { ...blog, url };
+                return {...blog, url};
             })
         );
 
         setBlogs(blogsWithMedia);
         setLoading(false)
     };
+
+    const getGalleries = async () => {
+        setLoading(true);
+        const res = await api.get('gallery');
+        const galleriesData = res.data;
+
+        // Fetch media for each blog
+        const galleriesWithMedia = await Promise.all(
+            galleriesData.map(async (gallery) => {
+                const mediaResponse = await fetchMedia(gallery._id);
+                const url = mediaResponse.data.url ?? 'default.png';
+                return {...gallery, url};
+            })
+        );
+
+        setGalleries(galleriesWithMedia)
+        setLoading(false)
+    }
 
     // Helper function to get name
     const getName = id => {
@@ -57,6 +76,10 @@ const GlobalContextProvider = ({ children }) => {
         setContacts(res.data);
     };
 
+    const getNeighbourhoodName = (id) => {
+        const neighbourhood = neighbourhoods.find((neighbourhood) => neighbourhood._id === id);
+        return neighbourhood ? neighbourhood.name : '';
+    }
     const format = (timestamp) => {
         const date = new Date(timestamp);
         return date.toLocaleDateString('en-US', {
@@ -70,7 +93,7 @@ const GlobalContextProvider = ({ children }) => {
         try {
             return await api.get(`/media/getMediaForOwner/${ownerId}`);
         } catch (error) {
-            return { data: [] };
+            return {data: []};
         }
     };
 
@@ -84,6 +107,8 @@ const GlobalContextProvider = ({ children }) => {
                 getContacts(),
                 format(),
                 getName(),
+                getGalleries(),
+                getNeighbourhoodName(),
             ]);
         };
         fetchData();
@@ -98,7 +123,9 @@ const GlobalContextProvider = ({ children }) => {
             loading,
             contacts,
             format,
-            getName
+            getName,
+            galleries,
+            getNeighbourhoodName,
         }}>
             {children}
         </GlobalContext.Provider>
