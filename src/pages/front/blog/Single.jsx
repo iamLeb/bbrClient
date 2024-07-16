d2f87c070b5d3f0ee3f9bb4445c44cb14bb53ca9
Faiza <salami.faiza15@gmail.com>
2024-07-16 15:40:11 -0500

    BBR-81

--- src/pages/front/blog/Single.jsx
+++ src/pages/front/blog/Single.jsx
@@ -1,31 +1,53 @@
-import { IoFolderOpenOutline } from "react-icons/io5";
-import { CiCalendar } from "react-icons/ci";
-import { AiOutlineComment } from "react-icons/ai";
-import { IoIosArrowRoundForward } from "react-icons/io";
+import React, { useContext, useEffect, useState } from 'react';
+import { IoFolderOpenOutline } from 'react-icons/io5';
+import { CiCalendar } from 'react-icons/ci';
+import { AiOutlineComment } from 'react-icons/ai';
+import { IoIosArrowRoundForward } from 'react-icons/io';
+import { useParams } from 'react-router-dom';
+import api from '../../../services/api.js';
+import GlobalContext from '../../../context/Global.js';
 
 const Single = () => {
+    let { id } = useParams();
+    const [blog, setBlog] = useState({});
+    const { getName, loading } = useContext(GlobalContext);
+
+    const getBlog = async (id) => {
+        try {
+            const res = await api.get(`/blog/${id}`);
+            setBlog(res.data);
+        } catch (error) {
+            console.error('Error fetching blog:', error);
+        }
+    };
+
+    useEffect(() => {
+        getBlog(id);
+    }, [id]);
+
     return (
         <section className={'container mx-auto'}>
             <div className={'md:flex justify-between gap-4 py-9 px-4 md:px-0 space-x-6'}>
+
                 <div className={'md:w-2/3'}>
                     <div className={'pb-9'}>
-                        <h1 className={'text-3xl font-bold'}>We are hiring ‘moderately,’ says Compass CEO</h1>
+                        <h1 className={'text-3xl font-bold'}>{blog.title}</h1>
                         <ul className={'flex space-x-4 items-center py-2 text-xs md:text-sm font-light'}>
                             <li>
                                 <div className={'flex space-x-2 items-center'}>
-                                    <IoFolderOpenOutline/>
-                                    <span>Apartment</span>
+                                    <IoFolderOpenOutline />
+                                    <span>{getName(blog.category)}</span>
                                 </div>
                             </li>
                             <li>
                                 <div className={'flex space-x-2 items-center'}>
-                                    <AiOutlineComment/>
+                                    <AiOutlineComment />
                                     <span>No Comments</span>
                                 </div>
                             </li>
                             <li>
                                 <div className={'flex space-x-2 items-center'}>
-                                    <CiCalendar/>
+                                    <CiCalendar />
                                     <span>February 15, 2022</span>
                                 </div>
                             </li>
@@ -34,25 +56,12 @@ const Single = () => {
 
                     <div className={'overflow-hidden rounded-lg'}>
                         <img className={'object-cover object-center'}
-                             src="https://dreamhomewp.themesflat.com/wp-content/uploads/2024/02/blog8_3.webp"
+                             src={blog.url}
                              alt="photo"/>
                     </div>
 
                     <div className={'pt-9 font-light'}>
-                        <p>
-                            The average contract interest rate for 30-year fixed-rate mortgages with conforming loan
-                            balances ($726,200 or less) decreased to 6.40% from 6.45%, with points falling to 0.59 from
-                            0.62 (including the origination fee) for loans with a 20% down payment. It had been over 7%
-                            just a month ago.
-                            The average contract interest rate for 30-year fixed-rate mortgages with conforming loan
-                            balances ($726,200 or less) decreased to 6.40% from 6.45%, with points falling to 0.59 from
-                            0.62 (including the origination fee) for loans with a 20% down payment. It had been over 7%
-                            just a month ago.
-                            The average contract interest rate for 30-year fixed-rate mortgages with conforming loan
-                            balances ($726,200 or less) decreased to 6.40% from 6.45%, with points falling to 0.59 from
-                            0.62 (including the origination fee) for loans with a 20% down payment. It had been over 7%
-                            just a month ago.
-                        </p>
+                        <p>{blog.content}</p>
                     </div>
                 </div>
 
@@ -63,21 +72,21 @@ const Single = () => {
                         <ul>
                             <li>
                                 <div className={'hover:text-primary flex justify-between space-x-2 items-center border-b'}>
-                                    <p className={'py-3'}>Apartment</p>
+                                    <p className={'py-3'}>{getName(blog.category)}</p>
                                     <IoIosArrowRoundForward size={28}/>
                                 </div>
                             </li>
 
                             <li>
                                 <div className={'hover:text-primary flex justify-between space-x-2 items-center border-b'}>
-                                    <p className={'py-3'}>Apartment</p>
+                                    <p className={'py-3'}>{getName(blog.category)}</p>
                                     <IoIosArrowRoundForward size={28}/>
                                 </div>
                             </li>
 
                             <li>
                                 <div className={'hover:text-primary flex justify-between space-x-2 items-center border-b'}>
-                                    <p className={'py-3'}>Apartment</p>
+                                    <p className={'py-3'}>{getName(blog.category)}</p>
                                     <IoIosArrowRoundForward size={28}/>
                                 </div>
                             </li>
